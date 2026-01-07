1f683a502f28c3d61556497dc32c55d89a7634c4
rajeevkumar75 <krrajeev0000000000@gmail.com>
2026-01-07 21:45:48 +0530

    updating feature engineering block

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,3 +1,10 @@
 #feature engineering:-
 
+def add_features(df):
+    df['balance_salary_ratio'] = df['balance'] / (df['estimated_salary'] + 1)
+    df['products_per_tenure'] = df['products_number'] / (df['tenure'] + 1)
+
+    df['high_value_customer'] = ((df['balance'] > 100000) &
+                                  (df['estimated_salary'] > 100000)).astype(int)
+    return df
 
