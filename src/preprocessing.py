17b1184c436b838154477b8f290c225d34bd6a3e
rajeevkumar75 <krrajeev0000000000@gmail.com>
2026-01-07 21:42:34 +0530

    commit for preprocessing update

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -9,4 +9,10 @@ def preprocess_data(df):
     df['gender'] = df['gender'].map({'Male': 1, 'Female': 0})
     df = pd.get_dummies(df, columns=['country'], drop_first=True)
 
-    
+    X = df.drop('churn', axis=1)
+    y = df['churn']
+
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    return X_scaled, y, scaler
