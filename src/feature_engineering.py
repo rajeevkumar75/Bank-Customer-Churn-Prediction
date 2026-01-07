d98f0575bcc21bcf3f008bea5f975085a7d71c85
rajeevkumar75 <krrajeev0000000000@gmail.com>
2026-01-07 21:50:06 +0530

    upating

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,10 +1,20 @@
 #feature engineering:-
 
-def add_features(df):
-    df['balance_salary_ratio'] = df['balance'] / (df['estimated_salary'] + 1)
-    df['products_per_tenure'] = df['products_number'] / (df['tenure'] + 1)
+import numpy as np
 
-    df['high_value_customer'] = ((df['balance'] > 100000) &
-                                  (df['estimated_salary'] > 100000)).astype(int)
-    return df
+def add_features(df):
+
+    # 1️ Financial strength & risk
+    df['balance_salary_ratio'] = df['balance'] / (df['estimated_salary'] + 1)
+    df['low_credit_score'] = (df['credit_score'] < 600).astype(int)
+    df['high_balance'] = (df['balance'] > 100000).astype(int)
+
+    # 2️ Customer engagement & behavior
+    df['products_per_tenure'] = df['products_number'] / (df['tenure'] + 1)
+    df['inactive_high_balance'] = (
+        (df['active_member'] == 0) & (df['balance'] > 50000)
+    ).astype(int)
+
+
+
 
