fcfdd24341174a03d223001552c731398a709bd7
rajeevkumar75 <krrajeev0000000000@gmail.com>
2026-01-07 21:41:44 +0530

    commit for preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,12 @@
 #use for preprocessing data before training:-
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+def preprocess_data(df):
+    df = df.drop(columns=['customer_id'])
+
+    df['gender'] = df['gender'].map({'Male': 1, 'Female': 0})
+    df = pd.get_dummies(df, columns=['country'], drop_first=True)
+
+    
