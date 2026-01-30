fe4bc39fb83b6a04dc1b87adae65b44cb7959fd0
Rajeev kumar <158702931+rajeevkumar75@users.noreply.github.com>
2026-01-30 17:05:09 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -3,7 +3,6 @@ from pycaret.classification import load_model, predict_model
 import os
 
 def run_prediction_pipeline(input_data_path, output_file="predictions.csv"):
-    # 1. Load the trained model
     model_path = "models/catboost_classifier_model"
     
     if not os.path.exists(f"{model_path}.pkl"):
@@ -11,19 +10,16 @@ def run_prediction_pipeline(input_data_path, output_file="predictions.csv"):
 
     print(f"📦 Loading model from {model_path}...")
     model = load_model(model_path)
-    
-    # 2. Load the data to predict on
     if isinstance(input_data_path, str):
         data = pd.read_csv(input_data_path)
     else:
         data = input_data_path
-
-    # 3. Generate Predictions
     print("🔮 Generating predictions...")
     predictions = predict_model(model, data=data)
     output_dir = "data/output"
     os.makedirs(output_dir, exist_ok=True)
     save_path = os.path.join(output_dir, output_file)
     predictions.to_csv(save_path, index=False)
+
 
     return save_path
